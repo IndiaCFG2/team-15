8610c2daf3207be37f7c3ffae0fe931c8ab17cd8
Satyam Kumar Jha <satyamjhakumar@gmail.com>
2020-08-08 18:55:13 +0530

    Added database model files

--- 321 foundation/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import User
-
-    #created_at = models.TimeField(_(u"Conversation Time"), auto_now_add=True, blank=True);
-	
