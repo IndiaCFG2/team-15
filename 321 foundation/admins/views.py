166bd1fbaf2b80357ed38b03a18c3f64372f63a9
neha <nehatodima27@gmail.com>
2020-08-08 17:31:43 +0530

    authentication of school and org

---
+++ 321 foundation/admins/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout,login,authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
+#from admins import templates 
+#from blog import templates
+
+def adminhome_view(request):
+	return render(request,"admins/adminshome.html",{})
+# Create your views here.
